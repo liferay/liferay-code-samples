4bde52a2776987b793a6efc8e3ca82913ea48692
Milen Dyankov <milen.dyankov@liferay.com>
2019-04-04 22:37:58 +0300

    build all script

---
+++ portal/7.1/java8/build.sh
@@ -0,0 +1,50 @@
+
+source ./check.sh
+
+cd $CODE_FOLDER;
+
+# build standalone projects
+
+for f in $(find ./* -name ".project_folder") 
+do
+    echo 
+    echo "=================================================="
+    echo "==>> Building standalone $PROJECT_PATH with Gradle"
+    echo "=================================================="
+    echo 
+    cd $STANDALONE_GRADLE/$PROJECT_PATH
+    rm -rf .gradle
+    ./gradlew clean assemble
+
+    echo 
+    echo "=================================================="
+    echo "==>> Building standalone $PROJECT_PATH with Maven"
+    echo "=================================================="
+    echo 
+    cd $STANDALONE_MAVEN/$PROJECT_PATH
+    mvn clean package
+done;
+
+
+# build workspaces
+
+echo 
+echo "=================================================="
+echo "==>> Building the whole Gradle workspace"
+echo "=================================================="
+echo 
+cd $WORKSPACE_GRADLE
+rm -rf .gradle
+./gradlew clean assemble;
+
+echo 
+echo "=================================================="
+echo "==>> Building the whole Maven workspace"
+echo "=================================================="
+echo 
+cd $WORKSPACE_MAVEN
+mvn clean package;
+
+
+
+cd $CURRENT_FOLDER;

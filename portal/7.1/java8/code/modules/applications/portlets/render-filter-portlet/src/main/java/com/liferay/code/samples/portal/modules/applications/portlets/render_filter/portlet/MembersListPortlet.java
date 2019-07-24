0c113a60085c3cafebd7b8b188aeb64e76ba05f0
David Gomez G <dgomezg@gmail.com>
2019-07-24 10:58:36 +0200

    Removed the ActionCommand implementation. It's not necesary to have a separate action command for just one action. Besides that, it's not the purpose of this code-sample to show MVCActionCommand use. Thus, the loadUsers() method has been moved to the portlet

--- portal/7.1/java8/code/modules/applications/portlets/render-filter-portlet/src/main/java/com/liferay/code/samples/portal/modules/applications/portlets/render_filter/portlet/MembersListPortlet.java
+++ portal/7.1/java8/code/modules/applications/portlets/render-filter-portlet/src/main/java/com/liferay/code/samples/portal/modules/applications/portlets/render_filter/portlet/MembersListPortlet.java
@@ -1,10 +1,16 @@
 package com.liferay.code.samples.portal.modules.applications.portlets.render_filter.portlet;
 
+import com.liferay.code.samples.portal.modules.applications.portlets.render_filter.model.Person;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
 
+import javax.portlet.ActionRequest;
+import javax.portlet.ActionResponse;
 import javax.portlet.Portlet;
 
 import org.osgi.service.component.annotations.Component;
+
+import java.util.Arrays;
+import java.util.List;
 
 @Component(
 	immediate = true,
@@ -25,5 +31,30 @@ public class MembersListPortlet extends MVCPortlet {
     public static final String MEMBERSLIST_PORTLET_NAME = "member_list_portlet";
     public static final String MEMBERLIST_ATTRIBUTE 	= "memberlist";
     public static final String LOAD_USERS_ACTION 		= "loadUsers";
+
+	/**
+	 * Method that simulates the loading of a list of users and puts it as an attribute of the ActionRequest so that
+	 *  it is available during the render phase.
+	 *
+	 *  In a real example, this user list will be probably fetched from a DB or from another system using a Service.
+	 *  To keep this example simple, a hard-coded list of data is returned.
+	 *
+	 *  Also, in a real scenario, when there is more than one action to be implemented, it is usually delegated through
+	 *  an MVCActionCommand (See MVCActionCommand code sample).
+	 */
+	public void loadUsers(ActionRequest actionRequest, ActionResponse actionResponse) {
+
+		actionRequest.setAttribute(MembersListPortlet.MEMBERLIST_ATTRIBUTE, createStaticUserList());
+
+	}
+
+	private List<Person> createStaticUserList() {
+		return Arrays.asList(
+				new Person("Sievert Shayne", "Sievert.Shayne@example.org"),
+				new Person("Vida Jonas", "Vida.Jonas@example.net"),
+				new Person("Nikola Septima", "Nikola.Septima@example.com"),
+				new Person("Ericka Merav", "Ericka.Merav@example.dev"),
+				new Person("Kennet Brandr", "Kennet.Brandr@example.com"));
+	}
 
 }
--- portal/7.2/java8/code/modules/applications/portlets/render-filter-portlet/src/main/java/com/liferay/code/samples/portal/modules/applications/portlets/render_filter/portlet/MembersListPortlet.java
+++ portal/7.2/java8/code/modules/applications/portlets/render-filter-portlet/src/main/java/com/liferay/code/samples/portal/modules/applications/portlets/render_filter/portlet/MembersListPortlet.java
@@ -1,12 +1,14 @@
 package com.liferay.code.samples.portal.modules.applications.portlets.render_filter.portlet;
 
+import com.liferay.code.samples.portal.modules.applications.portlets.render_filter.model.Person;
 import com.liferay.portal.kernel.portlet.bridges.mvc.MVCPortlet;
 
 import javax.portlet.*;
 
 import org.osgi.service.component.annotations.Component;
 
-import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
 @Component(
 	immediate = true,
@@ -27,5 +29,31 @@ public class MembersListPortlet extends MVCPortlet {
 	public static final String MEMBERSLIST_PORTLET_NAME 	= "render_filter_portlet";
 	public static final String MEMBERLIST_ATTRIBUTE 		= "memberlist";
 	public static final String LOAD_USERS_ACTION 			= "loadUsers";
+
+	/**
+	 * Method that simulates the loading of a list of users and puts it as an attribute of the ActionRequest so that
+	 *  it is available during the render phase.
+	 *
+	 *  In a real example, this user list will be probably fetched from a DB or from another system using a Service.
+	 *  To keep this example simple, a hard-coded list of data is returned.
+	 *
+	 *  Also, in a real scenario, when there is more than one action to be implemented, it is usually delegated through
+	 *  an MVCActionCommand (See MVCActionCommand code sample).
+	 */
+	public void loadUsers(ActionRequest actionRequest, ActionResponse actionResponse) {
+
+		actionRequest.setAttribute(MembersListPortlet.MEMBERLIST_ATTRIBUTE, createStaticUserList());
+
+	}
+
+	private List<Person> createStaticUserList() {
+		return Arrays.asList(
+				new Person("Sievert Shayne", "Sievert.Shayne@example.org"),
+				new Person("Vida Jonas", "Vida.Jonas@example.net"),
+				new Person("Nikola Septima", "Nikola.Septima@example.com"),
+				new Person("Ericka Merav", "Ericka.Merav@example.dev"),
+				new Person("Kennet Brandr", "Kennet.Brandr@example.com"));
+	}
+
 
 }
